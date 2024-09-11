d96e9ed252fe456deb8b9cad06f46daf7935baa9
Josh <jp.jacques@pm.me>
2024-09-11 12:02:39 -0500

    fin 1st part

---
+++ src/com/amazon/ata/executorservice/futures/UpdateRingTask.java
@@ -0,0 +1,22 @@
+package com.amazon.ata.executorservice.futures;
+
+import com.amazon.ata.executorservice.coralgenerated.devicecommunication.UpdateDeviceFirmwareRequest;
+import com.amazon.ata.executorservice.coralgenerated.devicecommunication.UpdateDeviceFirmwareResponse;
+import com.amazon.ata.executorservice.devicecommunication.RingDeviceCommunicatorService;
+
+import java.util.concurrent.Callable;
+
+public class UpdateRingTask implements Callable<UpdateDeviceFirmwareResponse> {
+    private UpdateDeviceFirmwareRequest request;
+    private RingDeviceCommunicatorService ringClient;
+
+    public UpdateRingTask(RingDeviceCommunicatorService ringClient, UpdateDeviceFirmwareRequest request) {
+        this.ringClient = ringClient;
+        this.request = request;
+    }
+
+    @Override
+    public UpdateDeviceFirmwareResponse call() {
+        return ringClient.updateDeviceFirmware(this.request);
+    }
+}
